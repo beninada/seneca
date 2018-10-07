e52a9f2278325652afa19aa71dd8e0da2b5455f5
Ben Inada <me@beninada.com>
2018-10-07 02:26:54 -0700

    Remove unnecessary ui stuff.

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,10 @@
 
 require('./bootstrap');
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
-require('./components/Example');
+import Example from './components/Example';
+
+render(<Example />, document.getElementById('example'));
